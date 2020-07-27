8ec4003037d32089a3af712662547ad959bfc534
Debasis Saikia <debasishsaikia136@gmail.com>
2020-07-27 15:14:22 +0530

    javascript modified

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ var pro_total=document.getElementById('pro_total');
 var ship_total=document.getElementById('ship_total')
 var total_cart_amt=document.getElementById('total_cart_amt');
 
-var discountcode=document.getElementById('discount_code1');
-
 const decreaseNumber=(addrem,price)=>{
 var itemval=document.getElementById(addrem);
 var price = document.getElementById(price);
@@ -39,14 +37,4 @@ const increaseNumber = (addrem,price) => {
   }
 };
 
- const discount_code=()=>{
-     let totalproamt=parseInt(total_cart_amt.innerHTML);
-     let error_trw=document.getElementById('error_trw');
-     if(discount_code.value === 'DEB123'){
-         let newtotalamount=totalproamt-15;
-         total_cart_amt.innerHTML=newtotalamount;
-         error_trw.innerHTML='Coupon code applied. You saved Rs 15 on this product'
-     }else{
-        error_trw.innerHTML="Invalid Coupon code";
-     }
- }
+ 
